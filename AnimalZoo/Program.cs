c8f719789e660c7fc48a92a5d7df70f0caba99b0
Linus Landin <linus.landin@edu.lycksele.se>
2020-11-12 11:51:33 +0100

    Started commenting AnimalZoo

--- AnimalZoo/Program.cs
+++ AnimalZoo/Program.cs
@@ -8,21 +8,21 @@ namespace AnimalZoo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Animal[] allAnimals;
+            Animal[] allAnimals; //The object "allAnimals" get sent to the class "Animal"
 
-            allAnimals = new Animal[4];
+            allAnimals = new Animal[4]; //4 empty Animals gets created in "allAnimals"
 
-            allAnimals[0] = new Cat("Kurre");
+            allAnimals[0] = new Cat("Kurre"); //A new cat gets created and put in the first spot in the array
 
-            allAnimals[1] = new Dog("Kenneth");
+            allAnimals[1] = new Dog("Kenneth"); //A new dog gets created and put in the second spot in the array
 
-            allAnimals[2] = new Cat("Bamse");
+            allAnimals[2] = new Cat("Bamse"); //A new cat gets created and put in the third spot in the array
 
-            allAnimals[3] = new Bird("Johnny");
+            allAnimals[3] = new Bird("Johnny"); //A new Bird gets created and put in the fourth spot in the array
 
-            foreach (Animal a  in allAnimals)
+            foreach (Animal a in allAnimals)
             {
                 Console.WriteLine("Nu kommer " + a.name);
                 a.IntroduceYourself();
